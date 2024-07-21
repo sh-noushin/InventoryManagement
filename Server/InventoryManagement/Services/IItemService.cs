f54c4b00a5bca565d71ea154cc5f002fb16c368f
Nooshin Shokrollahi <nshokrollahi951@gmail.com>
2024-07-21 16:18:13 +0200

    ItemService implemented

---
+++ Server/InventoryManagement/Services/IItemService.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagement.Models;
+
+namespace InventoryManagement.Services
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<Item>> GetAllItemsAsync();
+        Task<Item> GetItemByIdAsync(int id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(int id);
+    }
+}
